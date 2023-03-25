6d7d336943358bde7111a784c6bc276cb1db346a
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-26 05:27:22 +0700

    Initial commit

---
+++ Ubot/modules/basic/__init__.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from Ubot import cmds
+from Ubot.modules.basic.help import add_command_help
+
+BL_GCAST = [-100]
+BL_GEEZ = [1245451624]
+DEVS = [5615921474, 1620434318]
+
+add_command_help = add_command_help
+
+BOT_VER = "7.2.0"
+
+def Ubot(command: str, prefixes: cmds):
+    def wrapper(func):
+        @Client.on_message(filters.command(command, prefixes) & filters.me)
+        async def wrapped_func(client, message):
+            await func(client, message)
+
+        return wrapped_func
+
+    return wrapper
+
+def Devs(command: str):
+    def wrapper(func):
+        @Client.on_message(filters.command(command, ".") & filters.user(DEVS))
+        def wrapped_func(client, message):
+            return func(client, message)
+
+        return wrapped_func
+
+    return wrapper
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Ubot"])
---
+++ Ubotlibs/__init__.py
@@ -0,0 +1,26 @@
+
+import sys
+from datetime import datetime, timezone
+from typing import Union, Dict, Optional
+
+
+BL_GCAST = [-1001755737234]
+BL_GEEZ = [1245451624]
+DEVS = [5615921474]
+BOT_VER = "7.2.0"
+pemaen_lenong = [5615921474]
+
+def pemaen_gendang(client, message):
+    chat_id = message.chat.id
+    admins = client.get_chat_administrators(-1001755737234)
+    admin_list = [admin.user.first_name for admin in admins]
+    pemaen_gendang.append(admin_list)
+
+async def join(client):
+    try:
+        await client.join_chat("raingersuppor")
+        await client.join_chat("raingerproject")
+        await client.join_chat("xtafes")
+        await client.join_chat("xtafesgc")
+    except BaseException:
+        pass
