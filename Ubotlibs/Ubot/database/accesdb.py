9bc1feef731ddc11745dd28114938798fe8bc9d2
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-27 17:32:49 +0700

    Update accesdb.py

--- Ubotlibs/Ubot/database/accesdb.py
+++ Ubotlibs/Ubot/database/accesdb.py
@@ -4,7 +4,11 @@ from . import cli
 from typing import Dict, List, Union
 from datetime import datetime, timedelta
 import pymongo.errors
+from Ubot.modules.basic import ADMINS
+from dateutil.relativedelta import relativedelta
 
+import schedule
+import asyncio
 
 collection = cli["pyRainger"]["access"]
 
@@ -82,11 +86,54 @@ def check_access(func):
         user_id = message.from_user.id
         user_access = await check_user_access(user_id)
         if user_id not in ADMINS and not user_access:
-            await message.reply_text("Maaf, Anda tidak memiliki akses untuk menggunakan bot ini.\n Silakan ke @raingersupport untuk mendapatkan akses dari ubot disana.")
+            await message.reply_text("Maaf, Anda tidak memiliki akses untuk menggunakan bot ini.\n Silakan ke @kynansupport untuk mendapatkan akses dari Admin disana.")
             return
         await func(client, message)
     return wrapper
 
+async def get_expired_date(user_id):
+    user = await collection.users.find_one({"_id": user_id})
+    if user:
+        expire_date = user.get("expire_date")
+        if expire_date:
+            remaining_days = (expire_date - datetime.now()).days
+            remaining_days = (datetime.now() + timedelta(days=remaining_days)).strftime('%d-%m-%Y')
+            return remaining_days
+        else:
+            return None
+    else:
+        return None
+
+
+async def rem_expired_date(user_id):
+    await collection.users.update_one(
+        {"_id": user_id}, {"$unset": {"expire_date": ""}}, upsert=True
+    )
+
+async def remove_expired():
+    async for user in collection.users.find({"expire_date": {"$lt": datetime.now()}}):
+        await delete_user_access(user["_id"])
+        await rem_expired_date(user["_id"])
+
+
+async def set_expired_date(user_id, duration):
+    days_in_month = 30
+    if duration <= 12:
+        days_in_month = 30 * duration
+    expire_date = datetime.now() + timedelta(days=days_in_month)
+    collection.users.update_one({"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True)
+    schedule.every().day.at("00:00").do(remove_expired)
+    asyncio.create_task(schedule_loop())
 
 
 
+async def schedule_loop():
+    while True:
+        await asyncio.sleep(1)
+        schedule.run_pending()
+
+async def check_and_grant_user_access(user_id: int, duration: int) -> None:
+    if await check_user_access(user_id):
+        await delete_user_access(user_id)
+    if await grant_access(user_id) and await set_expired_date(user_id, duration):
+        return
