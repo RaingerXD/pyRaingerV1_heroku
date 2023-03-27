c3e35218db31c35d08c04eb16b0ccf3eb7a10dd0
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-28 06:46:43 +0700

    Update carbon.py

--- Ubot/modules/basic/carbon.py
+++ Ubot/modules/basic/carbon.py
@@ -17,7 +17,7 @@ from Ubotlibs.Ubot import Ubot
 from Ubot import aiosession
 from Ubotlibs.Ubot.helper.PyroHelpers import ReplyCheck
 from Ubot.modules.basic import add_command_help
-from Ubotlibs.Ubot.database.accesdb import *
+from Ubot.core.db.accesdb import *
 from Ubot import cmds
 
 async def make_carbon(code):
