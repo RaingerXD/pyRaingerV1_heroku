49f2207e9a64e9fab3e5a67da905e56c09190cdc
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-27 17:27:50 +0700

    Update __init__.py

--- Ubotlibs/Ubot/database/__init__.py
+++ Ubotlibs/Ubot/database/__init__.py
@@ -1,5 +1,3 @@
-
-
 import logging
 import motor.motor_asyncio
 import codecs
@@ -9,5 +7,9 @@ from string import ascii_lowercase
 from typing import Dict, List, Union
 
 from config import MONGO_URL
+
+from .usersdb import *
+from .accesdb import *
+from .accesdb import *
 
 cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
