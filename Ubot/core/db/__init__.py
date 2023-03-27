ef2c114c667d220fe013b8e3daf9ea00733166ac
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-28 06:43:32 +0700

    Update __init__.py

--- Ubot/core/db/__init__.py
+++ Ubot/core/db/__init__.py
@@ -1,10 +1,11 @@
-from Ubotlibs.Ubot.database import cli
+import logging
+import motor.motor_asyncio
+import codecs
+import pickle
+
+from string import ascii_lowercase
+from typing import Dict, List, Union
+
 from config import MONGO_URL
-from .usersdb import *
-from .accesdb import *
-from .notesdb import *
 
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client["pyRainger"]
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
