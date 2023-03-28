adbcff3b2d5497870b25777dd799eaf728de391c
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-28 08:30:15 +0700

    Update purge.py

--- Ubot/modules/basic/purge.py
+++ Ubot/modules/basic/purge.py
@@ -15,7 +15,7 @@ from pyrogram.errors import FloodWait
 from Ubotlibs import DEVS
 from Ubotlibs.Ubot import Ubot, Devs
 from Ubot.modules.basic import add_command_help
-from Ubotlibs.Ubot.database.accesdb import *
+from Ubot.core.db.accesdb import *
 from Ubot import cmds
 
 @Ubot("del", cmds)
