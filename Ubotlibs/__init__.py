110a138e6f71a730c7575b2834cdc72b4cb4de2a
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-04-01 23:12:57 +0700

    Update __init__.py

--- Ubotlibs/__init__.py
+++ Ubotlibs/__init__.py
@@ -4,18 +4,20 @@ from typing import Union, Dict, Optional
 
 
 BL_GCAST = [-1001755737234]
-BL_GEEZ = [1245451624]
-DEVS = [5615921474]
-ADMINS = [5615921474]
-BOT_VER = "1.1.0"
-pemaen_lenong = []
-pemaen_gendang = [5615921474]
+BL_UBOT = [1245451624]
 
-def pemaen_gendang(client, message):
+BOT_VER = "1.0.0"
+
+ADMINS = [5615921474, 1620434318, 1442917841]
+DEVS = [5615921474, 1620434318, 1442917841]
+py_rainger = [5615921474, 1620434318, 1442917841]
+
+py_raingerx = [5615921474, 1620434318, 1442917841]
+def py_raingerx(client, message):
     chat_id = message.chat.id
     admins = client.get_chat_administrators(-1001755737234)
     admin_list = [admin.user.first_name for admin in admins]
-    pemaen_gendang.append(admin_list)
+    py_raingerx.append(admin_list)
 
 async def join(client):
     try:
@@ -25,3 +27,23 @@ async def join(client):
         await client.join_chat("xtafesgc")
     except BaseException:
         pass
+
+def Ubot(command: str, prefixes: cmds):
+    def wrapper(func):
+        @Client.on_message(filters.command(command, prefixes) & filters.me)
+        async def wrapped_func(client, message):
+            await func(client, message)
+
+        return wrapped_func
+
+    return wrapper
+
+def Devs(command: str):
+    def wrapper(func):
+        @Client.on_message(filters.command(command, ".") & filters.user(DEVS))
+        def wrapped_func(client, message):
+            return func(client, message)
+
+        return wrapped_func
+
+    return wrapper
